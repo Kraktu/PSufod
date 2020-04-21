12c4d1e4554e16b4a685c2384320499763269547
Kraktu <57345348+Kraktu@users.noreply.github.com>
2020-04-21 13:55:07 +0200

    scene fond

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Orientation
+{
+	south,
+	southWest,
+	west,
+	northWest,
+	north,
+	northEast,
+	east,
+	southEast
+}
+public class Player : MonoBehaviour
+{
+	string userName;
+	int[] worldpos = new int[2];
+	int localPos;
+	Orientation myOrientation;
+}
